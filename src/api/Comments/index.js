5c3e3bfaabcd0205633be47b88a1bdd77d0f9841
Oleh Yelechko <lalka210767@gmail.com>
2022-07-14 23:14:51 +0300

    fixes

--- src/api/Comments/index.js
+++ src/api/Comments/index.js
@@ -2,19 +2,19 @@ import { instance } from "..";
 
 class Comment {
   async getAllComments() {
-    const comments = await instance.get(`/comments`);
+    const comments = await instance.get(`comments`);
     return comments
   }
   async getPostsComment(postId) {
-    const comments = await instance.get(`/comments/${postId}`);
+    const comments = await instance.get(`comments/${postId}`);
     return comments
   }
   async create(req) {
-    const create = await instance.post('/comments', req);
+    const create = await instance.post('comments', req);
     return create
   }
   async deleteComment(id) {
-    const data = await instance.delete(`/comments/${id}`);
+    const data = await instance.delete(`comments/${id}`);
     return data
   }
   async updateComment(req, id) {
@@ -22,7 +22,7 @@ class Comment {
       text: req.text,
       postId: req.postId
     }
-    const data = await instance.patch(`/comments/${id}`, comment);
+    const data = await instance.patch(`comments/${id}`, comment);
     return data
   }
 }
